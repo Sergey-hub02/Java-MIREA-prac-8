1388117facbb3e5a87bd9560f2f0d43dd94f9eab
Sergey-hub02 <s.park190802@gmail.com>
2021-10-13 15:28:59 +0300

    Написал интерфейс IWaitList и написал супер-класс WaitList, который реализует этот интерфейс

--- src/ru/mirea/classes/WaitList.java
+++ src/ru/mirea/classes/WaitList.java
@@ -1,4 +1,76 @@
 package ru.mirea.classes;
 
-public class WaitList {
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import ru.mirea.interfaces.IWaitList;
+
+public class WaitList<E> implements IWaitList<E> {
+  protected ConcurrentLinkedQueue<E> content;
+
+  /**
+   * Конструктор по умолчанию, создающий пустой список ожидания
+   */
+  public WaitList() {
+    this.content = new ConcurrentLinkedQueue<E>();
+  }
+
+  /**
+   * Создаёт список ожидания из переданной коллекции
+   * @param c       коллекция, из которой будет создан список ожидания
+   */
+  public WaitList(Collection<E> c) {
+    this.content = new ConcurrentLinkedQueue<E>(c);
+  }
+
+  /**
+   * Объединяет информацию об объекте в одну строку
+   * @return        общая информация об объекте
+   */
+  @Override
+  public String toString() {
+    return this.content.toString();
+  }
+
+  /**
+   * Добавляет элемент в конец списка ожидания
+   * @param element         добавляемый элемент
+   */
+  public void add(E element) {
+    this.content.add(element);
+  }
+
+  /**
+   * Возвращает и удаляет элемент из начала списка ожидания
+   * @return                первый элемент списка ожидания
+   */
+  public E remove() {
+    return this.content.poll();
+  }
+
+  /**
+   * Проверяет, есть ли в списке заданный элемент
+   * @param element         элемент, который нужно найти
+   * @return                признак успешного/безуспешного поиска
+   */
+  public boolean contains(E element) {
+    return this.content.contains(element);
+  }
+
+  /**
+   * Проверяет, содержит ли список все элементы из заданной коллекции
+   * @param c               элементы коллекции, которые нужно найти
+   * @return                признак успешного/безуспешного поиска
+   */
+  public boolean containsAll(Collection<E> c) {
+    return this.content.containsAll(c);
+  }
+
+  /**
+   * Проверяет, является ли список пустым
+   * @return          true - список пуст, false - в списке есть хотя бы 1 элемент
+   */
+  public boolean isEmpty() {
+    return this.content.isEmpty();
+  }
 }
--- src/ru/mirea/interfaces/IWaitList.java
+++ src/ru/mirea/interfaces/IWaitList.java
@@ -1,4 +1,37 @@
 package ru.mirea.interfaces;
 
-public interface IWaitList {
+import java.util.Collection;
+
+public interface IWaitList<E> {
+  /**
+   * Добавляет элемент в конец списка ожидания
+   * @param element         добавляемый элемент
+   */
+  void add(E element);
+
+  /**
+   * Возвращает и удаляет элемент из начала списка ожидания
+   * @return                первый элемент списка ожидания
+   */
+  E remove();
+
+  /**
+   * Проверяет, есть ли в списке заданный элемент
+   * @param element         элемент, который нужно найти
+   * @return                признак успешного/безуспешного поиска
+   */
+  boolean contains(E element);
+
+  /**
+   * Проверяет, содержит ли список все элементы из заданной коллекции
+   * @param c               элементы коллекции, которые нужно найти
+   * @return                признак успешного/безуспешного поиска
+   */
+  boolean containsAll(Collection<E> c);
+
+  /**
+   * Проверяет, является ли список пустым
+   * @return          true - список пуст, false - в списке есть хотя бы 1 элемент
+   */
+  boolean isEmpty();
 }
